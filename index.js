66af81e582e2a93f096bd16fd72d30c5df22a67b
Priyanshu Soni <114473997+priyanshuxdev@users.noreply.github.com>
2023-05-24 14:08:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 function calAge() {
 
+    const day_div = document.getElementById("day_div");
+    const month_div = document.getElementById("month_div");
+    const year_div = document.getElementById("year_div");
+
     const dayEl = document.getElementById("day").value;
     const monthEl = document.getElementById("month").value;
     const yearEl = document.getElementById("year").value;
@@ -7,6 +11,46 @@ function calAge() {
     const showYear = document.getElementById("YY");
     const showMonth = document.getElementById("MM");
     const showDay = document.getElementById("DD");
+
+
+    if (!dayEl || dayEl > 31) {
+        day_div.classList.add("invalid");
+        $("#day_div p").text("this field is required");
+
+        if (dayEl > 31) {
+            $("#day_div p").text("Must be a valid day");
+        }
+
+    } else {
+        day_div.classList.remove("invalid");
+    }
+
+    if (!monthEl || monthEl > 12) {
+        month_div.classList.add("invalid");
+        $("#month_div p").text("this field is required");
+
+        if (monthEl > 12) {
+            $("#month_div p").text("Must be a valid month");
+        }
+
+    } else {
+        month_div.classList.remove("invalid");
+    }
+
+    if (!yearEl || yearEl > 2100) {
+        year_div.classList.add("invalid");
+        $("#year_div p").text("this field is required");
+
+        if (yearEl > 2100) {
+            $("#year_div p").text("Must be in the past");
+        }
+
+    } else {
+        month_div.classList.remove("invalid");
+    }
+
+    if (!dayEl || !monthEl || !yearEl) return;
+
 
     const dob = new Date(yearEl, monthEl, dayEl);
 
@@ -17,40 +61,37 @@ function calAge() {
     const currentDate = new Date();
 
     const currentYear = currentDate.getFullYear();
-    const currentMonth = currentDate.getMonth();
+    const currentMonth = currentDate.getMonth() + 1;
     const currentDay = currentDate.getDate();
 
     let ageYear = currentYear - dobYear;
     let ageMonth = currentMonth - dobMonth;
     let ageDay = currentDay - dobDay;
 
-    if (ageMonth < 0 || ageMonth === 0 && currentDay < dobDay) {
+    if (ageMonth < 0 || ageMonth === 0) {
+
         ageYear--;
         let newAgeMonth = currentMonth + 12;
-        let newAgeDay = currentDay + 31;
-
         ageMonth = newAgeMonth - dobMonth;
-        ageDay = newAgeDay - dobDay;
+
+        if (currentDay < dobDay) {
+            let newAgeDay = currentDay + 31;
+            ageDay = newAgeDay - dobDay;
+        }
+
+    } else {
+        ageMonth = currentMonth - dobMonth;
+        ageDay = currentDay - dobDay;
     }
-
-    // function valid() {
-    //     let inputs = document.querySelectorAll("input");
-
-    //     inputs.forEach((i) => {
-    //         if (!i.value) {
-    //             i.style.borderColor = "red";
-    //             let smallT = document.querySelectorAll("small");
-    //             smallT.textContent = "This field is required"
-    //             smallT.classList.add("smlp");
-    //         }
-    //     });
-    // }
 
     showYear.textContent = ageYear;
     showMonth.textContent = ageMonth;
-    showDay.textContent = ageDay
-
-
-
-
+    showDay.textContent = ageDay;
 }
+
+
+// document.querySelectorAll("input").forEach(el => {
+//     el.addEventListener("input", () =>{
+//         calAge();
+//     })
+// });
